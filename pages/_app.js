ade1d7c416d1c926ff452e3ee3786de87a0b071a
shaik abbas <shaik.abbas@automate.io>
2021-09-02 19:33:05 +0530

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "tailwindcss/tailwind.css";
+import "../styles/globals.css";
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function About(){
+    return (
+        <>
+         <Head>
+            <title>About</title>
+            <meta name="description" content="This is profile page"/>
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <h1>About Screen</h1>
+        </>
+    )
+}
